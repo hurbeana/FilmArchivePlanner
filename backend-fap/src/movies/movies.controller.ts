31fa0f3697107eeb34790fec80d200e2214fcdab
Clemens <>
2021-11-26 01:13:18 +0100

    implement movie deletion backend

--- backend-fap/src/movies/movies.controller.ts
+++ backend-fap/src/movies/movies.controller.ts
@@ -1,13 +1,13 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Logger,
-  Param,
-  Post,
-  Put,
-  Query,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Logger,
+    Param,
+    Post,
+    Put,
+    Query,
 } from '@nestjs/common';
 import { MoviesService } from './movies.service';
 import { CreateUpdateMovieDto } from './dto/create-update-movie.dto';
@@ -20,47 +20,47 @@ import { SearchMoviePagingDto } from './dto/search-movie-paging.dto';
  */
 @Controller('movies')
 export class MoviesController {
-  constructor(private readonly moviesService: MoviesService) {}
+    constructor(private readonly moviesService: MoviesService) { }
 
-  private readonly logger = new Logger(MoviesController.name);
+    private readonly logger = new Logger(MoviesController.name);
 
-  @Post()
-  create(@Body() createMovieDto: CreateUpdateMovieDto) {
-    this.logger.log('Create movie called.');
-    return this.moviesService.create(createMovieDto);
-  }
+    @Post()
+    create(@Body() createMovieDto: CreateUpdateMovieDto) {
+        this.logger.log('Create movie called.');
+        return this.moviesService.create(createMovieDto);
+    }
 
-  @Get()
-  find(@Query() searchPaging: SearchMoviePagingDto) {
-    this.logger.log('Get movies called.');
-    this.logger.log(searchPaging);
-    const { limit, page, orderBy, sortOrder, searchstring, ...search } =
-      searchPaging;
-    const p: IPaginationOptions = {
-      page: page ?? 1,
-      limit: limit ?? 10,
-    };
-    return this.moviesService.find(p, search, orderBy, sortOrder, searchstring);
-  }
+    @Get()
+    find(@Query() searchPaging: SearchMoviePagingDto) {
+        this.logger.log('Get movies called.');
+        this.logger.log(searchPaging);
+        const { limit, page, orderBy, sortOrder, searchstring, ...search } =
+            searchPaging;
+        const p: IPaginationOptions = {
+            page: page ?? 1,
+            limit: limit ?? 10,
+        };
+        return this.moviesService.find(p, search, orderBy, sortOrder, searchstring);
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: number) {
-    this.logger.log(`Get movie with id ${id} called.`);
-    return this.moviesService.findOne(id);
-  }
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        this.logger.log(`Get movie with id ${id} called.`);
+        return this.moviesService.findOne(id);
+    }
 
-  @Put(':id')
-  update(
-    @Param('id') id: number,
-    @Body() updateMovieDto: CreateUpdateMovieDto,
-  ) {
-    this.logger.log(`Update movie with id ${id} called.`);
-    return this.moviesService.update(id, updateMovieDto);
-  }
+    @Put(':id')
+    update(
+        @Param('id') id: number,
+        @Body() updateMovieDto: CreateUpdateMovieDto,
+    ) {
+        this.logger.log(`Update movie with id ${id} called.`);
+        return this.moviesService.update(id, updateMovieDto);
+    }
 
-  @Delete(':id')
-  remove(@Param('id') id: number) {
-    this.logger.log(`Delete movie with id ${id} called.`);
-    return this.moviesService.remove(id);
-  }
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        this.logger.log(`Delete movie with id ${id} called.`);
+        return this.moviesService.delete(id);
+    }
 }
