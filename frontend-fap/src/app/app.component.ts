c86fee5bc11df838ac238369257312442c8b68b3
Sebastian Luzian <e1327570@student.tuwien.ac.at>
2021-12-08 21:18:17 +0000

    bugfix/renaming

--- frontend-fap/src/app/app.component.ts
+++ frontend-fap/src/app/app.component.ts
@@ -1,20 +1,17 @@
-import {Component} from '@angular/core';
-import {Store} from "@ngrx/store";
-import * as MovieActions from "./movies/state/movies.actions";
-import {Title} from "@angular/platform-browser";
+import { Component } from '@angular/core';
+import { Store } from '@ngrx/store';
+import * as MovieActions from './movies/state/movies.actions';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.less']
+  styleUrls: ['./app.component.less'],
 })
 export class AppComponent {
-  title = 'Festivator';
+  title = 'Film Archive Planner';
 
-
-  constructor(
-    private titleService: Title
-  ) {}
+  constructor(private titleService: Title) {}
 
   ngOnInit() {
     this.titleService.setTitle(this.title); // set page title
